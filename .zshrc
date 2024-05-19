57acef207d42be526dd0e8a2d62c1f011eee40a4
vedaant-rajoo <vedaant12345@gmail.com>
2024-05-19 04:31:23 -0700

    chore: Update dotfile symlinking script

--- .zshrc
+++ .zshrc
@@ -31,3 +31,7 @@ alias lt='ls --tree'
 alias cd='z'
 
 eval "$(zoxide init zsh)"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- configure.sh
+++ configure.sh
@@ -1,8 +1,30 @@
-#! /bin/bash
+#!/bin/bash
 
-DOTFILES=(.gitconfig .gitignore .zshrc)
+# Define the path to your dotfiles repository
+DOTFILES_REPO="$HOME/dotfyles"
 
-for dotfile in $(echo ${DOTFILES[*]});
-do
-    cp ~/dotfyles/$(echo $dotfile) ~/$(echo $dotfile)
+# List of dotfiles you want to symlink
+declare -a dotfiles=(
+    ".zshrc"
+    ".gitignore"
+    ".gitconfig"
+    # Add other dotfiles here
+)
+
+# Create symlinks for each dotfile
+for file in "${dotfiles[@]}"; do
+    target="$HOME/$file"
+    source="$DOTFILES_REPO/$file"
+    
+    # Backup existing dotfile if it exists
+    if [ -e "$target" ]; then
+        mv "$target" "$target.bak"
+        echo "Moved existing $file to $file.bak"
+    fi
+    
+    # Create symlink
+    ln -s "$source" "$target"
+    echo "Created symlink for $file"
 done
+
+echo "All dotfiles have been symlinked."
